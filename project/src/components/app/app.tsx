2f7c4fdb9a7c26a31a16f09563060402431136d3
Alonesa <alenakrasnov@mail.ru>
2021-10-12 16:24:50 +0300

    added routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import Error from '../error-404/error';
 
 const FILM_INFO = {
   name: 'The Grand Budapest Hotel',
@@ -12,8 +14,19 @@ const FILM_INFO = {
 
 function App(): JSX.Element {
   return (
-    <Main name={FILM_INFO.name} genre={FILM_INFO.genre} release={FILM_INFO.release} posterImage={FILM_INFO.posterImage} backgroundImage={FILM_INFO.backgroundImage}/>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main name={FILM_INFO.name} genre={FILM_INFO.genre} release={FILM_INFO.release} posterImage={FILM_INFO.posterImage} backgroundImage={FILM_INFO.backgroundImage}/>
+        </Route>
+        <Route path="/login">
+          <SignIn/>
+        </Route>
+        <Route>
+          <Error/>
+        </Route>
+      </Switch>
+    </BrowserRouter>  );
 }
 
 export default App;
